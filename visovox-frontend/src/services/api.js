f1e773f514452bd35d4836dd1ce8e1301e80f5f7
Lupin <ogunsanya.a.akanbi@gmail.com>
2025-07-02 16:04:12 +0100

    modified my api.js to ensure that proper API call is instantiated

--- visovox-frontend/src/services/api.js
+++ visovox-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const uploadImage = async (formData, type) => {
-  const endpoint = `${BASE_URL}/${type}`;
+  const endpoint = `${BASE_URL}/${type}/`;
   const response = await fetch(endpoint, {
     method: 'POST',
     body: formData,
